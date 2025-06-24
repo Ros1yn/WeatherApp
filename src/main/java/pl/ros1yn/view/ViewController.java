82bf1a359ca11f56426c85f628c204611ac8b0f7
Mateusz <mk.kwiatkowski30@gmail.com>
2025-06-24 14:31:09 +0200

    Added a simple web interface

--- src/main/java/pl/ros1yn/view/ViewController.java
+++ src/main/java/pl/ros1yn/view/ViewController.java
@@ -1,16 +1,39 @@
 package pl.ros1yn.view;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import pl.ros1yn.model.Weather;
+import pl.ros1yn.service.WeatherService;
 
 @Controller
+@RequiredArgsConstructor
 public class ViewController {
+
+    private final WeatherService weatherService;
+
+    private static final String WEATHER = "weather";
 
     @GetMapping("/weather")
     public String home(Model model) {
         model.addAttribute("title", "Weather☀️");
-        model.addAttribute("message", "Witaj w Thymeleaf!");
-        return "weather";
+        return WEATHER;
+    }
+
+    @PostMapping("/weather")
+    public String getWeather(@RequestParam String city, Model model) {
+        ResponseEntity<Weather> response = weatherService.getWeatherByCity(city);
+        if (response.getStatusCode().is2xxSuccessful()) {
+            model.addAttribute(WEATHER, response.getBody());
+
+            return "weather-result";
+        } else {
+            model.addAttribute("error", "City was not found, try again.");
+            return WEATHER;
+        }
     }
 }
